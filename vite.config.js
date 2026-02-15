4077d69b3ecfdfa8ff84b45e61420631f717f2c9
a6a27 <a6a27shen@gmail.com>
2026-02-15 23:06:35 +0800

    Fix Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/food-choice/',
+  base: '/FoodChoice/',
   plugins: [react()],
   optimizeDeps: {
     include: ['sql.js']
